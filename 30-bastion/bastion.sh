3558cd0be7f597ea49acfd51d3b8aadca69ae512
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-31 23:36:40 +0530

    changes done

---
+++ 30-bastion/bastion.sh
@@ -0,0 +1,8 @@
+growpart /dev/nvme0n1 4
+lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+
--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -3,6 +3,19 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = local.bastion_sg_id
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data = file("bastion.sh")
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 50
+    tags = merge(
+      {
+          Name = "${var.project}-${var.env}-bastion"
+      },
+      local.common_tags
+    )
+  }
 
   tags = merge(
     local.common_tags,
@@ -10,4 +23,41 @@ resource "aws_instance" "example" {
         Name = "${var.project}-${var.env}-bastion"
     }
   )
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "RoboShopDevBastion"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    {
+      Name = "RoboShopDevBastion"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.project}-${var.env}-bastion"
+  role = aws_iam_role.role.name
 }
