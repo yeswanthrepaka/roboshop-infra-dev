a3e94afe8e802cc2890218a46610b28c214e5f15
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-04-02 15:49:56 +0530

    changes done

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -117,3 +117,43 @@ resource "terraform_data" "mysql" {
        ]
     }
 }
+
+# Rabbitmq instance creation and connecting from bastion host
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [ local.rabbitmq_sg_id ]
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.env}-rabbitmq"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [ 
+    aws_instance.rabbitmq.id 
+    ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+    provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+      inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+       ]
+    }
+}
--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -16,3 +16,20 @@ resource "aws_route53_record" "redis" {
   allow_overwrite = true
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = local.zone_id
+  name    = "rabbitmq-${var.env}.${local.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = local.zone_id
+  name    = "mysql-${var.env}.${local.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
