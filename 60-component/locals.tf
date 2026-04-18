c1898d2df690c4399221f7aa7bb5c05c4397c8ed
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-04-18 11:09:38 +0530

    changes done

--- 60-component/locals.tf
+++ 60-component/locals.tf
@@ -1,7 +1,7 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids)[0]
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
         Project = var.project
