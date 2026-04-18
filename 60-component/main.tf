30fdc5f78d98a1c3b908d761ffa36e2fab91351d
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-04-18 12:35:39 +0530

    changes done

--- 60-component/main.tf
+++ 60-component/main.tf
@@ -185,7 +185,7 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-resource "terraform_data" "main" {
+resource "terraform_data" "main_delete" {
   triggers_replace = [
     aws_instance.main.id
   ]
