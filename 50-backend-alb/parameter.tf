98e61f38990c5966bccccb3c492bcb7f92a1f0b4
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-04-18 12:40:18 +0530

    changes done

--- 50-backend-alb/parameter.tf
+++ 50-backend-alb/parameter.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "backend_alb_arn" {
+resource "aws_ssm_parameter" "backend_alb_listener_arn" {
   name = "/${var.project}/${var.env}/backend_alb_listener_arn"
   type = "String"
   value = aws_lb_listener.backend_alb.arn
