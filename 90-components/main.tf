fd3def4706db2a66024c35b6d3f5cbea75ed4ee2
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-04-20 12:29:29 +0530

    changes done

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -2,5 +2,5 @@ module "components" {
     for_each = var.component
   source = "git::https://github.com/yeswanthrepaka/terraform-aws-component.git?ref=main"
   component = each.key
-  rule_priority = each.value
+  rule_priority = each.value.rule_priority
 }
