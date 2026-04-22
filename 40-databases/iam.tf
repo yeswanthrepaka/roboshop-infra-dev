fde3c14776f3a5a3048f03f685636ae8bc93e841
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-04-23 01:31:24 +0530

    changes done

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -28,9 +28,7 @@ resource "aws_iam_role" "mysql" {
 resource "aws_iam_policy" "mysql" {
   name        = local.mysql_policy_name
   description = "A policy for MySQL Ec2 instance"
-  policy      = templatefile("mysql-iam-policy.json", {
-                environment = var.env
-  })
+  policy      = file("mysql-iam-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "mysql" {
